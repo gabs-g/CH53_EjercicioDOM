d9c51eb973f4a7cc0d240e4a7a4da2ae5c4a6ef3
anagabriela <anagarcia.comunicacion@gmail.com>
2025-04-08 12:18:54 -0600

    Se realizan pruebas con DOM

--- README.md
+++ README.md
@@ -17,6 +17,6 @@ git commit -m "mensaje"
 git push origin main
 
 ```
->Vista previa del index.html
+> Vista previa del index.html
 
 ![index](https://raw.githubusercontent.com/gabs-g/CH53_EjercicioDOM/refs/heads/main/image/Captura%20de%20pantalla%202025-04-08%20a%20la(s)%2010.47.15.png)
--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 let btnMostrar= document.getElementById("btnMostrar");
 let encabezado1 = document.getElementById("encabezado1");
 let encabezado2 = document.getElementById("encabezado2");
-let lista = document.getElementsByTagName("ul");
+let listas = document.getElementsByTagName("ul");
 let elementos = document.getElementsByClassName("list-group-item");
 let otroElemento = document.querySelector("ul>li");
 let otrosElementos = document.querySelectorAll("ul>li");
 
 let contador = 0;
 
-console.log(lista.length);
-console.log(lista[0]);
-console.log(lista.item(1))
+console.log(listas.length);
+console.log(listas[0]);
+console.log(listas.item(1))
 console.log(elementos.length);
 console.log(elementos.item(2));
 console.log("otroElemento:", otroElemento);
@@ -26,5 +26,14 @@ console.log(encabezado2.innerText);
 
 btnMostrar.addEventListener("click", function(event){
     event.preventDefault(); //no hagas loq ue haces por defecto
-    console.log("botón btnModificar presionado");
+
+    let element = document.createElement("li");
+    element.innerText="Another item"; // <li> Another item</li>
+element.classList.add("list-group-item");
+
+    let element2 = element.cloneNode (true);
+
+    listas.item(0).append(element) //Inserta el elemento al final de la lsita
+    listas.item(0).after(element) // Inserta el elemento después de la lista
+
 });
